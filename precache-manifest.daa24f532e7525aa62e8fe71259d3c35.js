c8b2ab99223a44e8237616dbd6b5537393c6da8b
supriya <linkin.suppu@gmail.com>
2020-05-15 09:44:24 +0545

    Updates

---
+++ precache-manifest.daa24f532e7525aa62e8fe71259d3c35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3776fbaba7a220372335131eda2ac5ee",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9cd470fcbe6e2d206294",
+    "url": "/react-app/static/css/main.1f0e7994.chunk.css"
+  },
+  {
+    "revision": "ae0d0c1987197c7aba33",
+    "url": "/react-app/static/js/2.42f1aab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.42f1aab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd470fcbe6e2d206294",
+    "url": "/react-app/static/js/main.d685b0aa.chunk.js"
+  },
+  {
+    "revision": "8ebe45bc5548c20d999e",
+    "url": "/react-app/static/js/runtime-main.92951d66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
